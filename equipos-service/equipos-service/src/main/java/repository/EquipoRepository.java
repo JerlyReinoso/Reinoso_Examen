7490c87163e762f356e0a1ac5c03142852c1233d
Jerly Fernanda Reinoso Namicela <75386318+JerlyReinoso@users.noreply.github.com>
2025-01-27 19:57:18 -0500

    Delete equipos-service/equipos-service directory

--- equipos-service/equipos-service/src/main/java/repository/EquipoRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Equipo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EquipoRepository extends JpaRepository<Equipo, Long> {
-}
