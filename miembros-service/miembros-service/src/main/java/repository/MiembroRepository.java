f25fe6abd6d7384a71519444376c287d7941c9ba
Jerly Fernanda Reinoso Namicela <75386318+JerlyReinoso@users.noreply.github.com>
2025-01-27 19:57:30 -0500

    Delete miembros-service/miembros-service directory

--- miembros-service/miembros-service/src/main/java/repository/MiembroRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Miembro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MiembroRepository extends JpaRepository<Miembro, Long> {
-}
